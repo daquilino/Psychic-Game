03d901dff0de131882d70e42d2bc7543d7cb7fb0
douglas aquilino <douglas aquilino>
2017-01-31 14:20:59 -0500

    Completed JS Coding. Basic HTML layout.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,41 +2,46 @@
 /*  RCB - "Psychic-Game" Homework #3           */
 /*                game.js                      */
 
+	// Global Variable Declarations
+	var wins = 0;
+	var losses = 0;
+	var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+	var guessesLeft = 10;
+	var guessesMade = [];
+	var playerGuess = "";
+	var psychicGuess = "";
 
-var wins = 0;
-var losses = 0;
-var letters = "abcdefghijklmnopqrstuvwxyz".split("");
-var guessesLeft = 10;
-var guessesMade = [];
-var playerGuess = "";
-var psychicGuess = "";
-
+	//==============================================================
 	
+	// Event Handler. Monitors for 'key up'.
+	// Assigns playerGuess that key.		
+	// Error checks key is a letter.
+	// Assigns psychicGuess random letter via randomLetter()
+	// Calls compare() for game logic.
+	// Calls showResults to display game results to player.
 	document.onkeyup = function(element){
-
 		
-		playerGuess = event.key.toLowerCase();
+		playerGuess = event.key.toUpperCase();
 		
+		// checks if key pressed is a letter.
 		if (letters.indexOf(playerGuess) < 0){
 			alert("Woops! Please Guess a Letter!");
-			return;			
+			return 0;			
 		}
 		
-console.log("pre: " + psychicGuess); //test code
+		psychicGuess = randomLetter();
 		
-		psychicGuess = (randomLetter(letters));
+		compare();
 		
-console.log("post: " + psychicGuess); //test code
-		
-		compare(playerGuess, psychicGuess, wins, losses, guessesMade, guessesLeft);
-		
-		showResults(wins, losses, guessesLeft, guessesMade);
+		showResults();
 	
 	}//END document.onkeyup = function(element)
 
-
-	//When passed and array; randomly chooses then returns
-	// element
+	//==============================================================
+	
+	//Checks if new game (i.e. guessesLeft = 10).
+	//If so returns random letter from letters[].
+	//If not returns current psychicGuess value. 
 	function randomLetter(){
 		
 		if (guessesLeft === 10){
@@ -47,34 +52,39 @@ console.log("post: " + psychicGuess); //test code
 		}
 	}// END randomLetter()
 
-
-	// Logic for comparing guesses and ...
+	//==============================================================
+	
+	// Compares playerGuess to psychicGuess.
+	// If true displays win alert, increments 'Wins', and resets game.
+	// If false decrements 'Guesses Left'. Then checks if still more
+	// guesses availible.  If yes pushes guess to "Your Guesses So Far".
+	// If not alerts you lose, increments 'Losses', and resets game.	
 	function compare(){
 
-		if (guessesLeft > 1){	
+		if(playerGuess === psychicGuess){
+			winAlert();
+			wins++;
+			resetGame();
+		}
+		else {
+			guessesLeft--;		
+			
+			if(guessesLeft === 0){
 
-			if(playerGuess === psychicGuess){
-				alert("You Win!!!");
-				wins++;
-				guessesLeft = 10;
-				guessesMade = [];
+				alert("You Lose! My Letter Was: " + "'"+  psychicGuess + "'" + " Try Again.");			
+				losses++;
+				resetGame();
 			}
-			else {
-				guessesLeft--;
+			else{
 				guessesMade.push(playerGuess);
 			}
 		}
-		else{
-			alert("No More Guesses. You Lose! Try Again.");
-			losses++;
-			guessesLeft = 10;
-			guessesMade = [];
-			return;
-		}	
-
-	}// END compare()
-
-	// Displays to screen results of game.
+	
+	}// END compare()	
+	
+	//==============================================================
+	
+	// Displays results of game to page.
 	function showResults() {
 
 		var results = "<p>Wins: " + wins + "</p>" + "<p>Losses: " + losses + "</p>" + "<p>Guesses Left: " + guessesLeft + "</p>" + "<p>Your Guesses So Far: " + guessesMade + "</p>";
@@ -82,3 +92,29 @@ console.log("post: " + psychicGuess); //test code
 		document.querySelector("#results").innerHTML= results;
 
 	}// END showResults()
+	
+	//==============================================================
+	
+	//Displays a random win alert message.
+	function winAlert(){
+		
+		var message = ["Now order me a pizza.  WITH YOUR MIND!!!",
+			"Can you pick me up a Powerball ticket?",
+			"You, Me, Vegas!!!"	]; 
+		
+		var randMessage = message[Math.floor(Math.random()*message.length)];
+
+		alert("Your Psychic! " +  randMessage);
+	
+	}//END winAlert()
+
+	//==============================================================
+
+	//Resets game by setting 'Guesses Left' to ten and
+	//clearing 'Your Guesses So Far: '.  
+	function resetGame(){
+		
+		guessesLeft = 10;
+		guessesMade = [];
+
+	}//END resetGame()
